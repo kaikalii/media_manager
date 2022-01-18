1fd5e4ce9b49e9e0957cebb41d35c4aa1de43a0b
Kai Schmidt <kaikaliischmidt@gmail.com>
2022-01-18 13:13:01 -0800

    handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::PathBuf, process::exit};
+use std::{env, fs, io, path::PathBuf, process::exit};
 
 use clap::Parser;
 
@@ -11,17 +11,23 @@ struct App {
 fn main() {
     let app = App::parse();
 
-    println!("will set tags of {:?} to:", app.path);
-
-    let dir = std::env::var("MEDIA_PATH").unwrap_or_else(|_| {
+    let dir = env::var("MEDIA_PATH").unwrap_or_else(|_| {
         eprintln!("You must set MEDIA_PATH");
         exit(1)
     });
     let dir = PathBuf::from(dir);
 
-    for tag in app.tags {
+    let movie_path = app.path;
+    let copy_to_tag_folder = |tag: &str| -> io::Result<()> {
         let tag_dir = dir.join(tag);
-        let _ = fs::create_dir_all(&tag_dir);
-        let _ = fs::hard_link(&app.path, tag_dir.join(&app.path));
+        fs::create_dir_all(&tag_dir)?;
+        fs::hard_link(&movie_path, tag_dir.join(&movie_path))?;
+        Ok(())
+    };
+
+    for tag in app.tags {
+        if let Err(e) = copy_to_tag_folder(&tag) {
+            eprintln!("failed to copy to {} tag folder: {}", tag, e);
+        }
     }
 }
